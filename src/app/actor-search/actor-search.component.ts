40ff6106c44cab4727a82a4775920fe75e3e12a1
MaxStrandberg <Max.strandberg@outlook.com>
2020-09-08 16:10:55 +0300

    Created router and components

---
+++ src/app/actor-search/actor-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-actor-search',
+  templateUrl: './actor-search.component.html',
+  styleUrls: ['./actor-search.component.css']
+})
+export class ActorSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
+import { RouterModule } from '@angular/router';
+import { ActorSearchComponent } from './actor-search/actor-search.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MovieSearchComponent,
+    ActorSearchComponent,
+    FeedbackComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: 'movie-search', component: MovieSearchComponent},
+      {path: 'actor-search', component: ActorSearchComponent},
+      {path: 'feedback', component: FeedbackComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
