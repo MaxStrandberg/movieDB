fa07079b295feae69ae14935993b777b6305640a
MaxStrandberg <Max.strandberg@outlook.com>
2020-09-17 21:59:50 +0300

    changed timeout to delay so it works as it should

--- src/app/components/actor-search/actor-search.component.ts
+++ src/app/components/actor-search/actor-search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, timeout } from 'rxjs/operators';
+import { catchError, delay } from 'rxjs/operators';
 import { Observable, of, interval } from 'rxjs';
 import { moviedbkey } from '../../../environments/environment';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -29,25 +29,21 @@ export class ActorSearchComponent implements OnInit {
   }
 
   onKey(event) {
-    setTimeout(event, 250);
+
     const inputValue = event.target.value;
-    this.actorList = [];
     if (event.target.value.length > 0) {
-      this.getActors(inputValue).pipe().subscribe((data) => {
+      this.getActors(inputValue).pipe(delay(50)).subscribe((data) => {
         const results = Object.entries(data);
+        this.actorList = [];
         for (const result of results[3][1]) {
           if (result.known_for_department === 'Acting') {
             result.actorimg = 'https://image.tmdb.org/t/p/w92/' + result.profile_path;
             this.actorList.push(result);
-            console.log(this.actorList)
           } else {
             return;
           }
         }
       });
-
-
-      
     }
   }
   ngOnInit(): void {
