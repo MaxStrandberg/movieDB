57a08bdb7e860ddc84e74cd87a1febe13a088a0e
MaxStrandberg <Max.strandberg@outlook.com>
2020-09-24 17:16:14 +0300

    Added environments back from ignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: 'AIzaSyB_BVe_F_JCswDd_wXGSqgaIBPzxalohZM',
+    authDomain: 'moviedb-eac23.firebaseapp.com',
+    databaseURL: 'https://moviedb-eac23.firebaseio.com',
+    projectId: 'moviedb-eac23',
+    storageBucket: 'moviedb-eac23.appspot.com',
+    messagingSenderId: '737344771984',
+    appId: '1:737344771984:web:6880132572d868af2f8515',
+    measurementId: 'G-2980EXRC1H'
+  }
+};
+
+export const moviedbkey = '5168fd8e78b128991776c9d880afc6a5&query';
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
